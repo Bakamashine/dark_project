a9f11f890489c14edee3f1e2426a65dfde883f7a
Bakamashine <ivan.fa.002@gmail.com>
2026-05-29 17:10:35 +0300

    first commit

---
+++ forge.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+    },
+  ],
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hello from React + Electron + TypeScript!</h1>
+}
+
+export default App
