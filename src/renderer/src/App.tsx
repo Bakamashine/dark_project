23ca51cdf3949151db83b16017594b6273bf0680
Bakamashine <ivan.fa.002@gmail.com>
2026-06-02 17:10:28 +0300

    added electron builder

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages";
 import Project from "./pages/project";
 import BasicRedactorLayout from "./layouts/basic_redactor";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route element={<BasicRedactorLayout />}>
           <Route path="project/:project" element={<Project />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
